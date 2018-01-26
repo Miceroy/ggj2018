4ae185c3c7c7992edeb40a56cfc65f90729d19b7
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-26 22:24:48 +0200

    Up

--- Assets/scripts/CopterController.cs
+++ Assets/scripts/CopterController.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class CopterController : MonoBehaviour {
     public Vector2 speed = new Vector2(10,10);
     public float yawSpeed = 90.0f;
+    public CopterTilting tilt;
 
     Transform tr;
     // Use this for initialization
 	void Start () {
         tr = GetComponent<Transform>();
-	}
+     //   tilt = GetComponent<CopterTilting>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log("horizontal=" + horizontal.ToString());
-        Debug.Log("vertical=" + vertical.ToString());
-        Debug.Log("Time.deltaTime=" + Time.deltaTime.ToString());
-        Debug.Log("speed=" + speed.ToString());
+        tilt.updateTilt(vertical, horizontal);
+      //  Debug.Log("horizontal=" + horizontal.ToString());
+      //  Debug.Log("vertical=" + vertical.ToString());
+      //  Debug.Log("Time.deltaTime=" + Time.deltaTime.ToString());
+      //  Debug.Log("speed=" + speed.ToString());
         Vector3 delta = new Vector3(horizontal * speed.x * Time.deltaTime, 0, vertical * speed.y * Time.deltaTime);
-        Debug.Log("delta=" + delta.ToString());
+      //  Debug.Log("delta=" + delta.ToString());
         tr.Translate(delta);
 
         float yl = Input.GetAxis("YawLeft");
---
+++ Assets/scripts/CopterTilting.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopterTilting : MonoBehaviour {
+    public float rotateSpeed = 180;
+    public float maxAngle = 20;
+    public Transform tr;
+    float vert = 0;
+    float horiz = 0;
+    float curHor = 0;
+    float curVert = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    public void updateTilt(float vertical, float horizontal)
+    {
+        horiz = horizontal;
+        vert = vertical;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Mathf.Abs(horiz) > 0.01)
+        {
+            curHor -= rotateSpeed * horiz * Time.deltaTime;
+        }
+        else
+        {
+            if (curHor > 0.1)
+            {
+                curHor -= rotateSpeed * Time.deltaTime;
+            }
+            else if (curHor < -0.1)
+            {
+                curHor += rotateSpeed * Time.deltaTime;
+            }
+        }
+
+        if (Mathf.Abs(vert) > 0.01)
+        {
+            curVert += rotateSpeed * vert * Time.deltaTime;
+        }
+        else
+        {
+            if (curVert >= rotateSpeed * Time.deltaTime)
+            {
+                curVert -= rotateSpeed * Time.deltaTime;
+            }
+            else if (curVert <= -rotateSpeed * Time.deltaTime)
+            {
+                curVert += rotateSpeed * Time.deltaTime;
+            }
+        }
+
+        curHor = Mathf.Clamp(curHor, -maxAngle, maxAngle);
+        curVert = Mathf.Clamp(curVert, -maxAngle, maxAngle);
+        // rotate x = eteenpäin/taaksepäin
+        // rotate z = vasen/oikea
+        tr.eulerAngles = new Vector3(curVert, 0, curHor);
+     //   Debug.Log("tr.eulerAngles=" + tr.eulerAngles.ToString());
+    }
+}
