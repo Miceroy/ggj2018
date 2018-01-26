57050ea14d6f9af08e2c257f9bcfd122a739d167
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-26 23:37:41 +0200

    Lisätty WaypointTraveller.

---
+++ Assets/scripts/WaypointTraveller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointTraveller : MonoBehaviour {
+    public float travelSpeed = 1.0f;
+    public Transform [] waypoints;
+    int nextIndex = 0;
+    Transform tr;
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        tr.position = waypoints[0].position;
+        ++nextIndex;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distToDest = (waypoints[nextIndex].position - tr.position).magnitude;
+        if (distToDest < (travelSpeed * Time.deltaTime))
+        {
+            tr.position = waypoints[nextIndex].position;
+            ++nextIndex;
+        }
+        else
+        {
+            Vector3 v = (waypoints[nextIndex].position - tr.position).normalized;
+            Quaternion q = Quaternion.FromToRotation(transform.right, v);
+            tr.rotation = q * transform.rotation;
+        }
+
+        if (nextIndex < waypoints.Length)
+        {
+            Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex].position - tr.position).normalized;
+            tr.position += delta;
+        }
+    }
+}
