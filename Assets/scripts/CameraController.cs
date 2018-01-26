82030a956571857d24e6a97c718b7fc4c718e9ca
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-26 19:41:49 +0200

    Kopterin liikutus toimii.

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Transform trY;
+    public Transform trX;
+    public Vector2 speed = new Vector2(100, 100);
+    // Use this for initialization
+    void Start () {
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        trY.Rotate(0, Time.deltaTime*Input.GetAxis("Mouse X")* speed.x, 0);
+        trX.Rotate(-Time.deltaTime * Input.GetAxis("Mouse Y")* speed.y, 0, 0);
+    }
 }
--- Assets/scripts/CopterController.cs
+++ Assets/scripts/CopterController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CopterController : MonoBehaviour {
+    public Vector2 speed = new Vector2(10,10);
 
-	// Use this for initialization
+    Transform tr;
+    // Use this for initialization
 	void Start () {
-		
+        tr = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log("horizontal=" + horizontal.ToString());
+        Debug.Log("vertical=" + vertical.ToString());
+        Debug.Log("Time.deltaTime=" + Time.deltaTime.ToString());
+        Debug.Log("speed=" + speed.ToString());
+        Vector3 delta = new Vector3(horizontal * speed.x * Time.deltaTime, 0, vertical * speed.y * Time.deltaTime);
+        Debug.Log("delta=" + delta.ToString());
+        tr.Translate(delta);
+    }
 }
