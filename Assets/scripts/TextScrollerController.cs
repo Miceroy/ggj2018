43cbd8edc226662202702396d3038e13ecb8ed0f
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-27 16:17:02 +0200

    Lisätty ScrollingTextUI ja TextScrollerController.

---
+++ Assets/scripts/TextScrollerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScrollerController : MonoBehaviour {
+    public RectTransform obj;
+
+	// Use this for initialization
+	void Start () {
+        obj.anchoredPosition = new Vector2(600, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        obj.anchoredPosition -= new Vector2(100 * Time.deltaTime,0);
+        if (obj.anchoredPosition.x < -1000)
+        {
+            obj.anchoredPosition = new Vector2(600, 0);
+        }
+    }
+}
