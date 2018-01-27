12a7d870392bc0193f285a81293fad06507c5ada
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-27 22:16:02 +0200

    Lisätty game_main

--- Assets/scripts/RaycastHit.cs
+++ Assets/scripts/RaycastHit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RaycastHit : MonoBehaviour {
     public Camera cam;
     float totalFilmedTime = 0;
-
+    int hitsCount = 0;
     // Use this for initialization
     void Start () {
 		
@@ -15,12 +15,17 @@ public class RaycastHit : MonoBehaviour {
     void Update()
     {
         UnityEngine.Ray ray = cam.ScreenPointToRay(new Vector3(1280 / 2, 720 / 2, 0));
-        UnityEngine.RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        UnityEngine.RaycastHit [] hits = Physics.RaycastAll(ray);
+        bool nowHitting = false;
+     //   Debug.Log("hits.Length:"+ hits.Length);
+        for (int i=0; i< hits.Length; ++i)
         {
-            TargetToFilm ttf = hit.transform.gameObject.GetComponent<TargetToFilm>();
+            TargetToFilm ttf = hits[i].transform.gameObject.GetComponent<TargetToFilm>();
             if (ttf)
             {
+                //Debug.Log("Now hitting" + hitsCount);
+                ++hitsCount;
+                nowHitting = true;
                 totalFilmedTime += Time.deltaTime;
                 if(totalFilmedTime > ttf.timeToFilm)
                 {
@@ -29,8 +34,10 @@ public class RaycastHit : MonoBehaviour {
                 }
             }
         }
-        else
+
+        if(nowHitting == false)
         {
+         //   Debug.Log("Now hitting" + hits);
             totalFilmedTime -= Time.deltaTime;
             if (totalFilmedTime < 0) totalFilmedTime = 0;
         }
