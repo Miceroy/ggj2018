8f30322d16c68e8ab3243ef6f729d8ed26e7a4be
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-28 11:39:04 +0200

    Lisätty pilvenpiirtäjän kontrolleri.

---
+++ Assets/scripts/SkyscraperController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyscraperController : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (
+            other.gameObject.transform.parent &&
+            other.gameObject.transform.parent.parent &&
+            other.gameObject.transform.parent.parent.parent &&
+            other.gameObject.transform.parent.parent.parent.parent )
+        {
+            CopterController cop = other.gameObject.transform.parent.parent.parent.parent.GetComponent<CopterController>();
+            if (cop)
+            {
+                Vector3 ve = cop.gameObject.transform.position - transform.position;
+                ve.y = 0;
+                cop.addCollisionVelocity(ve.normalized * 50.0f);
+            }
+        }
+    }
+}
