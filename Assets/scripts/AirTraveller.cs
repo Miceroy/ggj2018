69c6a6b7b33a6ef03162918059e2a0b3580a5d30
vesinokkaelain <a_seppanen@hotmail.com>
2018-01-28 05:17:22 +0200

    Merge scene

--- Assets/scripts/AirTraveller.cs
+++ Assets/scripts/AirTraveller.cs
@@ -12,10 +12,12 @@ using UnityEngine;
 
 public class AirTraveller : MonoBehaviour {
     public float travelSpeed = 1.0f;
-    List<Vector3> waypoints = new List<Vector3>();
+    List<Transform> waypoints = new List<Transform>();
     int nextIndex = 0;
     Transform tr;
 	Transform ap;
+	float WaitTime = 0;
+	bool isMoving = true;
 	
 	// Use this for initialization
 	void Start () {		
@@ -23,13 +25,18 @@ public class AirTraveller : MonoBehaviour {
 		foreach (GameObject airpoint in GameObject.FindGameObjectsWithTag("BirdPath"))
 		{
 			ap = airpoint.GetComponent<Transform>();
-			waypoints.Add(ap.position);
+			waypoints.Add(ap);
+		}
+		foreach (GameObject airpoint in GameObject.FindGameObjectsWithTag("RoofPath"))
+		{
+			ap = airpoint.GetComponent<Transform>();
+			waypoints.Add(ap);
 		}
 		
         tr = GetComponent<Transform>();
 		
 		nextIndex = Random.Range(0,(waypoints.Count -1));
-        tr.position = waypoints[nextIndex];
+        tr.position = waypoints[nextIndex].position;
         
 		
 	}
@@ -37,29 +44,54 @@ public class AirTraveller : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+		if (isMoving)
+		{	
+			Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex].position - tr.position).normalized;
+            tr.position += delta;		
 
-        float distToDest = (waypoints[nextIndex] - tr.position).magnitude;
-        if (distToDest < (travelSpeed * Time.deltaTime))
-        {
-            tr.position = waypoints[nextIndex];
-			nextIndex = Random.Range(0,(waypoints.Count -1));
-        }
-        else
-        {
-            Vector3 v = (waypoints[nextIndex] - tr.position).normalized;
-            Quaternion q = Quaternion.FromToRotation(transform.right, v);
-            tr.rotation = q * transform.rotation;
-        }
+			float distToDest = (waypoints[nextIndex].position - tr.position).magnitude;
+			if (distToDest < (travelSpeed * Time.deltaTime))
+			{
+				tr.position = waypoints[nextIndex].position;
+				if (waypoints[nextIndex].gameObject.tag == "RoofPath")
+				{
+					WaitTime = Random.Range(2,6);
+					isMoving = false;
+					
+				}
+				else 
+				{
+					WaitTime = 0;
+					
+				}
+				Invoke("FlyToNextPoint", WaitTime);	
+			}
+			else
+			{
+				Vector3 v = (waypoints[nextIndex].position - tr.position).normalized;
+				Quaternion q = Quaternion.FromToRotation(transform.right, v);
+				tr.rotation = q * transform.rotation;				
+			}
+			
+			
+			
+		}
 
         //if (nextIndex < waypoints.Length)
         //{
-            Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex] - tr.position).normalized;
-            tr.position += delta;
+	}
+		
+	void FlyToNextPoint()
+		{   
+			nextIndex = Random.Range(0,(waypoints.Count -1));
+			
+			isMoving = true;
+		}
         /*}
         else
         {
             tr.position = waypoints[0].position;
             nextIndex = 1;
         }*/
-    }
 }
+
