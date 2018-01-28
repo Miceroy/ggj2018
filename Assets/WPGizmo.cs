27e5c5554b96caf5271b34e54cde75b00c939948
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-28 11:11:06 +0200

    CarTraveller

---
+++ Assets/WPGizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class WPGizmo : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 2);
+    }
+}
