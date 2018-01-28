d8dc2f08a368b2883212312fc43f7e68e60e78c8
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-28 10:12:37 +0200

    nolla takasi

--- Assets/scripts/SeagullController.cs
+++ Assets/scripts/SeagullController.cs
@@ -30,12 +30,9 @@ public class SeagullController : MonoBehaviour {
             CopterController cop = other.gameObject.transform.parent.parent.parent.parent.GetComponent<CopterController>();
             if (cop)
             {
-                //  Debug.Log("OnTriggerEnter: "+ (cop.gameObject.transform.position - other.transform.position).ToString());
-                //Debug.Log("OnTriggerEnter: " + cop.gameObject.name/*transform.position.ToString()*/ + " ," + other.gameObject.name/*.transform.position.ToString()*/);
                 Vector3 ve = cop.gameObject.transform.position - transform.position;
                 ve.y = 0;
                 cop.addCollisionVelocity(ve.normalized * 30.0f);
-                // cop.addCollisionVelocity(new Vector3(20,0,0));
             }
         }
     }
