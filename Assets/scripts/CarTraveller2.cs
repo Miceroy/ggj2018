5dda0478c94caf0febcb1e37b76d72e1efe9ed65
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-28 13:17:07 +0200

    Lisätty fileäj. Car 2

---
+++ Assets/scripts/CarTraveller2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTraveller2 : MonoBehaviour {
+    public GameObject wpRoot;
+    public float travelSpeed = 1.0f;
+    List<Transform> waypoints = new List<Transform>();
+    int nextIndex = 0;
+    Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        while (true)
+        {
+            Transform ap = wpRoot.transform.Find("WP ("+i+")");
+            if (ap == null) break;
+            waypoints.Add(ap);
+            ++i;
+        }
+        Debug.Log("Found " + i + " waypoints");
+        
+        tr = GetComponent<Transform>();		
+		nextIndex = 0;
+        tr.position = waypoints[nextIndex].position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		float distToDest = (waypoints[nextIndex].position - tr.position).magnitude;
+		if (distToDest < (travelSpeed * Time.deltaTime))
+		{
+				
+			tr.position = waypoints[nextIndex].position;
+		    ++nextIndex;
+            if (nextIndex >= waypoints.Count)
+                nextIndex = 0;			
+		}
+		else
+		{
+			Vector3 v = (waypoints[nextIndex].position - tr.position).normalized;
+			Quaternion q = Quaternion.FromToRotation(transform.right, v);
+			tr.rotation = q * transform.rotation;				
+		}
+			
+		Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex].position - tr.position).normalized;
+        tr.position += delta;
+	}
+}
+
