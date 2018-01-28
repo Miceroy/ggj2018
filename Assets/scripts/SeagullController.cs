ca31ce15b7621d398ec9dacd2283e4467ec48d49
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-01-28 10:02:59 +0200

    Bug fix in copter slide at seagull collision.

--- Assets/scripts/SeagullController.cs
+++ Assets/scripts/SeagullController.cs
@@ -10,24 +10,34 @@ public class SeagullController : MonoBehaviour {
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-//	void Update () {
-//	}
+
+    // Update is called once per frame
+    //	void Update () {
+    //	}
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("SeagullController::OnTriggerEnter");
+    //    Debug.Log("SeagullController::OnTriggerEnter");
         pulu.SetActive(false);
         particle.SetActive(true);
         Invoke("deleteMe", 5.0f);
-        CopterController cop = other.gameObject.transform.parent.parent.parent.parent.GetComponent<CopterController>();
-        //  Debug.Log("OnTriggerEnter: "+ (cop.gameObject.transform.position - other.transform.position).ToString());
-        //Debug.Log("OnTriggerEnter: " + cop.gameObject.name/*transform.position.ToString()*/ + " ," + other.gameObject.name/*.transform.position.ToString()*/);
-        Vector3 ve = cop.gameObject.transform.position - transform.position;
-        ve.y = 0;
-        cop.addCollisionVelocity(ve.normalized*20.0f);
-       // cop.addCollisionVelocity(new Vector3(20,0,0));
+        if (
+            other.gameObject.transform.parent &&
+            other.gameObject.transform.parent.parent &&
+            other.gameObject.transform.parent.parent.parent &&
+            other.gameObject.transform.parent.parent.parent.parent )
+        {
+            CopterController cop = other.gameObject.transform.parent.parent.parent.parent.GetComponent<CopterController>();
+            if (cop)
+            {
+                //  Debug.Log("OnTriggerEnter: "+ (cop.gameObject.transform.position - other.transform.position).ToString());
+                //Debug.Log("OnTriggerEnter: " + cop.gameObject.name/*transform.position.ToString()*/ + " ," + other.gameObject.name/*.transform.position.ToString()*/);
+                Vector3 ve = cop.gameObject.transform.position - transform.position;
+                ve.y = 0;
+                cop.addCollisionVelocity(ve.normalized * 30.0f);
+                // cop.addCollisionVelocity(new Vector3(20,0,0));
+            }
+        }
     }
 
     void deleteMe()
