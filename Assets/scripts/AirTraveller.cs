af2708452c7b989b358b37865e3aed552fea6901
vesinokkaelain <a_seppanen@hotmail.com>
2018-01-28 08:43:47 +0200

    Linnut toimii

--- Assets/scripts/AirTraveller.cs
+++ Assets/scripts/AirTraveller.cs
@@ -18,7 +18,7 @@ public class AirTraveller : MonoBehaviour {
 	Transform ap;
 	float WaitTime = 0;
 	bool isMoving = true;
-	
+	int vanhaIndex = 0;
 	// Use this for initialization
 	void Start () {		
 	
@@ -37,7 +37,12 @@ public class AirTraveller : MonoBehaviour {
 		
 		nextIndex = Random.Range(0,(waypoints.Count -1));
         tr.position = waypoints[nextIndex].position;
-        
+        vanhaIndex = nextIndex;
+		nextIndex = Random.Range(0,(waypoints.Count -1));
+			while (nextIndex == vanhaIndex)
+			{
+				nextIndex = Random.Range(0,(waypoints.Count -1));
+			}
 		
 	}
 
@@ -46,25 +51,31 @@ public class AirTraveller : MonoBehaviour {
     {
 		if (isMoving)
 		{	
-			Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex].position - tr.position).normalized;
-            tr.position += delta;		
+				
 
 			float distToDest = (waypoints[nextIndex].position - tr.position).magnitude;
 			if (distToDest < (travelSpeed * Time.deltaTime))
 			{
+				
 				tr.position = waypoints[nextIndex].position;
 				if (waypoints[nextIndex].gameObject.tag == "RoofPath")
-				{
-					WaitTime = Random.Range(2,6);
-					isMoving = false;
-					
+				{					
+					WaitTime = Random.Range(5,10);
+					isMoving = false;						
 				}
 				else 
-				{
+				{					
 					WaitTime = 0;
-					
+					isMoving = true;						
 				}
-				Invoke("FlyToNextPoint", WaitTime);	
+				vanhaIndex = nextIndex;
+				nextIndex = Random.Range(0,(waypoints.Count -1));
+				while (nextIndex == vanhaIndex)
+				{
+					nextIndex = Random.Range(0,(waypoints.Count -1));
+				}
+				Invoke("FlyToNextPoint", WaitTime);
+				
 			}
 			else
 			{
@@ -73,7 +84,8 @@ public class AirTraveller : MonoBehaviour {
 				tr.rotation = q * transform.rotation;				
 			}
 			
-			
+			Vector3 delta = travelSpeed * Time.deltaTime * (waypoints[nextIndex].position - tr.position).normalized;
+            tr.position += delta;	
 			
 		}
 
@@ -82,9 +94,7 @@ public class AirTraveller : MonoBehaviour {
 	}
 		
 	void FlyToNextPoint()
-		{   
-			nextIndex = Random.Range(0,(waypoints.Count -1));
-			
+		{   			
 			isMoving = true;
 		}
         /*}
